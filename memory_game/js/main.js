f2e31b6298d33e264825d74f8b03882951f0098b
Eliza <eliza@elizas-macbook-pro-4.local>
2017-10-13 17:48:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,25 @@
 console.log("Up and running!");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) { 
-	if (cardsInPlay[0] === cardsInPlay[2]) {
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) { if (cardsInPlay[0] === cardsInPlay[2]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
+	checkForMatch();
+}	
 }
-
+flipCard(0);
+flipCard(2);
 
