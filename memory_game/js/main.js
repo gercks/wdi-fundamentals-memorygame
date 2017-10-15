93360699b01b90ea09655e6c2fc2d0151b517aef
Eliza <eliza@elizas-macbook-pro-4.local>
2017-10-15 19:51:01 -0400

    finished game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,12 +26,14 @@ console.log("You found a match!");
 console.log("Sorry, try again.");
 }
 }
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
 		cardsInPlay.push(cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) { 
 	if (cardsInPlay[0] === cardsInPlay[2]) {
 	alert("You found a match!");
@@ -41,6 +43,24 @@ if (cardsInPlay.length === 2) {
 	checkForMatch();
 }	
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+
+resetBoard = function () {
+	var button = document.querySelector('button');
+	button.addEventListener('click', createBoard);
+}
+
+createBoard();
+
+
 
